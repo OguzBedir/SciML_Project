e20ef53de60b2d084b27ad52b949c2d853124cf4
oguzbedir <oguzbedir@hotmail.com>
2024-05-05 21:23:23 -0500

    Implement 2D and 3D plotting, add option for visualizing PINN approximation during training

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import numpy as  np
-
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
 
 from modules.model import PINN
 from modules.loss import Loss
 from modules.training import train_model
-from modules.utils import initial_condition, running_average
-from modules.domain_utils import get_initial_points
-from modules.plotting_utils import plot_color, plot_3D
+from modules.utils import initial_condition
 
 import os
 import yaml
@@ -40,7 +35,6 @@ device = (
     else "cpu"
 )
 
-
 pinn = PINN(LAYERS, NEURONS_PER_LAYER, act=nn.Tanh()).to(device)
 
 x_domain = [0.0, LENGTH]
@@ -61,7 +55,8 @@ loss_fn = Loss(
 )
 
 pinn_trained, loss_values, residual_loss_values, initial_loss_values, boundary_loss_values = train_model(
-    pinn, loss_fn=loss_fn, learning_rate=LEARNING_RATE, max_epochs=EPOCHS)
+    nn_approximator=pinn, loss_fn=loss_fn, learning_rate=LEARNING_RATE, max_epochs=EPOCHS, 
+    plot_solution=True, compute_device=device)
 
 pinn = pinn.cpu()
 losses = loss_fn.verbose(pinn)
@@ -69,60 +64,3 @@ print(f'Total loss: \t{losses[0]:.5f} ({losses[0]:.3E})')
 print(f'Interior loss: \t{losses[1]:.5f} ({losses[1]:.3E})')
 print(f'Initial loss: \t{losses[2]:.5f} ({losses[2]:.3E})')
 print(f'Bondary loss: \t{losses[3]:.5f} ({losses[3]:.3E})')
-
-
-# Plotting
-
-# Loss function
-# average_loss = running_average(loss_values, window=100)
-# fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
-# ax.set_title("Loss function (runnig average)")
-# ax.set_xlabel("Epoch")
-# ax.set_ylabel("Loss")
-# ax.plot(average_loss)
-# ax.set_yscale('log')
-# plt.show()
-
-# average_loss = running_average(residual_loss_values, window=100)
-# fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
-# ax.set_title("Residual loss function (running average)")
-# ax.set_xlabel("Epoch")
-# ax.set_ylabel("Loss")
-# ax.plot(average_loss)
-# ax.set_yscale('log')
-
-# average_loss = running_average(initial_loss_values, window=100)
-# fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
-# ax.set_title("Initial loss function (running average)")
-# ax.set_xlabel("Epoch")
-# ax.set_ylabel("Loss")
-# ax.plot(average_loss)
-# ax.set_yscale('log')
-
-# average_loss = running_average(boundary_loss_values, window=100)
-# fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
-# ax.set_title("Boundary loss function (running average)")
-# ax.set_xlabel("Epoch")
-# ax.set_ylabel("Loss")
-# ax.plot(average_loss)
-# ax.set_yscale('log')
-
-# Plotting
-# Initial condition
-base_dir = '.'
-x, y, _ = get_initial_points(x_domain, y_domain, t_domain, N_POINTS_PLOT, requires_grad=False)
-z = initial_condition(x, y)
-fig_color = plot_color(z, x, y, N_POINTS_PLOT, N_POINTS_PLOT, "Initial condition - exact")
-fig_3D = plot_3D(z, x, y, N_POINTS_PLOT, N_POINTS_PLOT, "Initial condition - exact")
-
-def plot(idx, t_value):
-    t = torch.full_like(x, t_value)
-    z = pinn(x, y, t)
-    fig_color = plot_color(z, x, y, N_POINTS_PLOT, N_POINTS_PLOT, f"PINN for t = {t_value}")
-    fig_3D = plot_3D(z, x, y, N_POINTS_PLOT, N_POINTS_PLOT, f"PINN for t = {t_value}")
-
-time_values = np.arange(0, TOTAL_TIME, 0.01)
-for idx, t_val in enumerate(time_values):
-    plot(idx, t_val)
-
-plt.show()
--- modules/training.py
+++ modules/training.py
@@ -3,12 +3,40 @@ import numpy as np
 import torch
 from typing import Callable
 from modules.model import PINN
+from modules.plotting_utils import plot_2D, plot_3D
+
+
+# >>> START <<<
+"""
+Quick and dirty solution.
+Can be refactored in the future.
+"""
+import os
+import yaml
+# Get the current directory
+current_dir = os.path.dirname(__file__)
+# Navigate to the parent directory
+parent_dir = os.path.dirname(current_dir)
+# Specify the path to your YAML file in the parent directory
+yaml_file_path = os.path.join(parent_dir, "config.yaml")
+# Open the YAML file and load its contents
+with open(yaml_file_path, "r") as file:
+    yaml_data = yaml.safe_load(file)
+# Now you can access elements from the loaded YAML data
+LENGTH = yaml_data["LENGTH"]
+TOTAL_TIME = yaml_data["TOTAL_TIME"]
+# >>> END <<<
+
+x_domain = [0.0, LENGTH]
+y_domain = [0.0, LENGTH]
 
 def train_model(
+    compute_device: str,
     nn_approximator: PINN,
     loss_fn: Callable,
     learning_rate: int = 0.01,
-    max_epochs: int = 1_000
+    max_epochs: int = 1_000,
+    plot_solution: bool = False,
 ) -> PINN:
 
     optimizer = torch.optim.Adam(nn_approximator.parameters(), lr=learning_rate)
@@ -20,9 +48,7 @@ def train_model(
     start_time = time.time()
 
     for epoch in range(max_epochs):
-
         try:
-
             loss: torch.Tensor = loss_fn(nn_approximator)
             optimizer.zero_grad()
             loss[0].backward()
@@ -32,13 +58,38 @@ def train_model(
             residual_loss_values.append(loss[1].item())
             initial_loss_values.append(loss[2].item())
             boundary_loss_values.append(loss[3].item())
+
+            if plot_solution and (epoch + 1) % 500 == 0:
+                # TODO: 
+                # - ADD AN IF STATEMENT FOR SELECTING 2D OR 3D PLOTTING OPTION
+                # - CHANGE STATIC 500 TO SOMETHING THAT CAN BE PASSED AS AN ARGUMENT
+                plot_3D(nn_approximator, x_domain, y_domain, t=0,
+                        fname=f"{epoch+1}, t={0}.png", device=compute_device)
+                plot_3D(nn_approximator, x_domain, y_domain, t=0.25,
+                        fname=f"{epoch+1}, t={0.25}.png", device=compute_device)
+                plot_3D(nn_approximator, x_domain, y_domain, t=0.5,
+                        fname=f"{epoch+1}, t={0.5}.png", device=compute_device)
+
+
+
             if (epoch + 1) % 1000 == 0:
                 epoch_time = time.time() - start_time
                 start_time = time.time()
 
-                print(f"Epoch: {epoch + 1} - Loss: {float(loss[0].item()):>7f}, Residual Loss: {float(loss[1].item()):>7f}, Initital Loss: {float(loss[2].item()):>7f}, Boundary Loss: {float(loss[3].item()):>7f}")
+                print(f"Epoch: {epoch + 1} - "
+                    f"Loss: {float(loss[0].item()):>7f}, "
+                    f"Residual Loss: {float(loss[1].item()):>7f}, "
+                    f"Initial Loss: {float(loss[2].item()):>7f}, "
+                    f"Boundary Loss: {float(loss[3].item()):>7f}, "
+                    f"Epoch Time: {epoch_time:.4f} seconds")
 
         except KeyboardInterrupt:
             break
 
-    return nn_approximator, np.array(loss_values), np.array(residual_loss_values), np.array(initial_loss_values), np.array(boundary_loss_values)
+    return (
+        nn_approximator,
+        np.array(loss_values),
+        np.array(residual_loss_values),
+        np.array(initial_loss_values),
+        np.array(boundary_loss_values)
+    )
